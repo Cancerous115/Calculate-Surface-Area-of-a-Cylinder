2ee62fa0c31fe8cefe79011819e9d0b1f05050da
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2024-01-18 09:55:16 -0500

    Add files via upload

---
+++ Mod2E1.py
@@ -0,0 +1,14 @@
+#Do what's in parenthesis first, then left to right
+#Pemmdas. For b has "7/2" will give a decimal answer of "3.5." Don't change just use it in the problem to get our answer.
+a = (2 - 4 + 12 / 4 + (5 * 7))
+b = 3+a*(7/2)
+#3.5 * a"36.0" then we add three to those sums for our answer.
+print(b)
+
+pi = 3.142
+radius = float(input("Enter radius: "))
+height = float(input("Enter height: "))
+# Surface Area of a Cylinder Formula ("2pi*r*h + 2pi*r^2)
+area = 2 * pi * radius * height+(2*pi*(radius*radius))
+area = float(area)
+print(area)
